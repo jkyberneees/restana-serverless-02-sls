b30b21ba7ebaf476164614cdc93f1654e8df89d4
Rolando Santamaria Maso <rsmaso@amazon.com>
2021-05-17 00:04:03 +0200

    updating dependencies

--- service.js
+++ service.js
@@ -1,20 +1,20 @@
 'use strict'
 
 const jsonParser = require('serverless-json-parser')()
-const query = require('connect-query')
 const service = require('restana')()
 
-service.use(query())
 service.use(jsonParser)
 
 service.get('/hi', (req, res) => {
   res.send({
-    msg: 'Hello World from restana service!'
+    msg: 'Hello World from API service!'
   })
 })
 
 service.post('/create', (req, res) => {
-  res.send(204)
+  res.send({
+    status: 'created'
+  }, 204)
 })
 
 module.exports = service
